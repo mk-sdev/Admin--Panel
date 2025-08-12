836879ab4e1ff0c729258b8372a38989c65872a0
Mikołaj <kaleta222@gmail.com>
2025-08-12 11:48:26 +0200

    feat(home): display users

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
+type SelectedUser = {
+  _id: string;
+  email: string;
+  roles: string[];
+  isVerified: boolean;
+  provider: string;
+} | null;
 @Component({
   selector: 'app-home',
+  imports: [CommonModule],
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  constructor(private auth: AuthService, private router: Router) {}
+export class HomeComponent implements OnInit {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  apiUrl = 'http://localhost:3000';
+
+  // display in the list
+  userItems: { _id: string; email: string }[] = [];
+  user: SelectedUser = null;
+
+  logEmail(_id: string) {
+    this.http
+      .get<SelectedUser>(this.apiUrl + '/protected/user/id/' + _id, {
+        withCredentials: true,
+      })
+      .subscribe((data) => {
+        this.user = data;
+      });
+  }
+
+  ngOnInit() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    this.http
+      .get<any[]>(this.apiUrl + '/protected/users', { withCredentials: true })
+      .subscribe({
+        next: (data) => {
+          console.log('Users fetched successfully:', data);
+          this.userItems = data;
+        },
+      });
+  }
 
   logout() {
     this.auth.logout().subscribe({
